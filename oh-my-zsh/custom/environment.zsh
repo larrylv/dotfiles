abf8cc2d50ef78fecdaf70d553da805bbb839847
Larry Lv <larrylv1990@gmail.com>
2012-12-23 04:22:55 +0800

    Add `.binstubs` to the PATH.
    I dropped rvm a few days ago, so shell won't add append BUNDLE_BIN path to the
    PATH when changing directories. And I always use `--binstubs=.binstubs`
    option for bundler. So it will be great to add `.binstubs` to the PATH.
    Thank god I won't type `bundle exec` or its alias `be` any more.

--- oh-my-zsh/custom/environment.zsh
+++ oh-my-zsh/custom/environment.zsh
@@ -8,6 +8,12 @@ SAVEHIST=HISTSIZE
 [[ -s "/opt/local/bin" ]] && export PATH=/opt/local/bin:$PATH
 [[ -s "/usr/local/bin" ]] && export PATH=/usr/local/bin:$PATH
 
+# I dropped rvm a few days ago, so shell won't add append BUNDLE_BIN path to the
+# PATH when changing directories. And I always use `--binstubs=.binstubs`
+# option for bundler. So it will be great to add `.binstubs` to the PATH.
+# Thank god I won't type `bundle exec` or its alias `be` any more.
+export PATH=.binstubs:$PATH
+
 export EDITOR=vim
 
 export LC_ALL=en_US.UTF-8
