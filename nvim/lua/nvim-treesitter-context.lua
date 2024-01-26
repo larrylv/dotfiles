0925a6bbe0345d0ed7b0648dda0ddf8781e9b090
Larry Lv <larrylv1990@gmail.com>
2024-01-26 14:45:50 -0800

    nvim-treesitter-context: set max lines to 5

--- nvim/lua/nvim-treesitter-context.lua
+++ nvim/lua/nvim-treesitter-context.lua
@@ -1,6 +1,6 @@
 require'treesitter-context'.setup{
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
     -- Separator between context and content. Should be a single character string, like '-'.
     -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
     separator = '-',
