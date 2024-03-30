fe66755be0cc2a9f6fb8055a333f673d93164316
Larry Lv <larrylv@openai.com>
2024-03-29 21:41:23 -0700

    treesitter: ensure yaml language is supported

--- nvim/lua/nvim-treesitter.lua
+++ nvim/lua/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "elixir", "go", "java", "javascript", "kotlin", "lua", "python", "ruby", "rust", "typescript" },
+  ensure_installed = { "c", "elixir", "go", "java", "javascript", "kotlin", "lua", "python", "ruby", "rust", "typescript", "yaml" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
