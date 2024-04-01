94482ff4977a8e4eab65ef48e16afa4027e0a359
Larry Lv <larrylv@openai.com>
2024-03-31 23:09:01 -0700

    nvim: use black, isort and ruff_fix as python formatters

--- nvim/lua/conform.lua
+++ nvim/lua/conform.lua
@@ -2,11 +2,11 @@ require("conform").setup({
   formatters_by_ft = {
     -- Conform will run multiple formatters sequentially
     -- python = { "autopep8", "isort", "ruff_format" },
-    python = {},
+    python = { "black", "isort", "ruff_fix" },
     ruby = { "rubyfmt" },
   },
   format_on_save = {
     lsp_fallback = true,
-    timeout_ms = 2000,
+    timeout_ms = 3000,
   },
 })
