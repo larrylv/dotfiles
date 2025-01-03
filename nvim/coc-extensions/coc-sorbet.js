40640b60ed5922ebfad41e6fc37269d09560b5c6
Larry Lv <larrylv@openai.com>
2025-01-02 22:42:15 -0800

    coc: disable sorbet for now

--- nvim/coc-extensions/coc-sorbet.js
+++ nvim/coc-extensions/coc-sorbet.js
@@ -44,35 +44,35 @@ async function getLspServices() {
   return services;
 }
 
-exports.activate = async context => {
-  var lspServices = await getLspServices()
-
-  if (lspServices != null) {
-    console.log("sorbet ready");
-
-    let cmds = [];
-    for (let scheme of SCHEMES) {
-      // manually set the autocmd with `sorbet:build/` because coc always sets
-      // up the autocmd with a `:/` suffix after the scheme, which won't work
-      // for `sorbet:build/${FILE_PATH}` files.
-      // Also disable ALE as rubocop & cocsorbet complains about a lot of things
-      // for the files not in our codebase.
-      cmds.push(`autocmd BufReadCmd,FileReadCmd,SourceCmd ${scheme}/* call coc#rpc#request('CocAutocmd', ['BufReadCmd','${scheme}', expand('<afile>')]) | ALEDisableBuffer`);
-      cmds.push(`autocmd BufReadCmd,FileReadCmd,SourceCmd ${scheme}:/* call coc#rpc#request('CocAutocmd', ['BufReadCmd','${scheme}', expand('<afile>')]) | ALEDisableBuffer`);
-      context.subscriptions.push(
-        workspace.registerTextDocumentContentProvider(
-          scheme,
-          new SorbetContentProvider(lspServices)
-        )
-      );
-    }
-
-    let content = `
-augroup coc_sorbet_dynamic_autocmd
-  autocmd!
-  ${cmds.join("\n  ")}
-augroup end`;
-
-    workspace.nvim.exec(content, false);
-  }
-}
+// exports.activate = async context => {
+//   var lspServices = await getLspServices()
+//
+//   if (lspServices != null) {
+//     console.log("sorbet ready");
+//
+//     let cmds = [];
+//     for (let scheme of SCHEMES) {
+//       // manually set the autocmd with `sorbet:build/` because coc always sets
+//       // up the autocmd with a `:/` suffix after the scheme, which won't work
+//       // for `sorbet:build/${FILE_PATH}` files.
+//       // Also disable ALE as rubocop & cocsorbet complains about a lot of things
+//       // for the files not in our codebase.
+//       cmds.push(`autocmd BufReadCmd,FileReadCmd,SourceCmd ${scheme}/* call coc#rpc#request('CocAutocmd', ['BufReadCmd','${scheme}', expand('<afile>')]) | ALEDisableBuffer`);
+//       cmds.push(`autocmd BufReadCmd,FileReadCmd,SourceCmd ${scheme}:/* call coc#rpc#request('CocAutocmd', ['BufReadCmd','${scheme}', expand('<afile>')]) | ALEDisableBuffer`);
+//       context.subscriptions.push(
+//         workspace.registerTextDocumentContentProvider(
+//           scheme,
+//           new SorbetContentProvider(lspServices)
+//         )
+//       );
+//     }
+//
+//     let content = `
+// augroup coc_sorbet_dynamic_autocmd
+//   autocmd!
+//   ${cmds.join("\n  ")}
+// augroup end`;
+//
+//     workspace.nvim.exec(content, false);
+//   }
+// }
